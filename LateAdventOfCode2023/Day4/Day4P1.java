5f8132867cec6d397f318eb10d8103af8f8be00a
dat <99101704+dat-tran05@users.noreply.github.com>
2023-01-12 10:13:55 -0600

    Advent of Code Day4

---
+++ LateAdventOfCode2023/Day4/Day4P1.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.*;
+import java.math.*;
+
+public class Day4P1{
+	public static void main(String[] nutz) throws IOException {
+		Scanner sc = new Scanner(new File("Day4/input.txt"));
+//		
+	}
+}
